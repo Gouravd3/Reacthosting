3f5255a13d8d961c0c239a3b44a1c11dfa81f588
Gouravd3 <165121973+Gouravd3@users.noreply.github.com>
2024-05-18 23:20:20 +0530

    git first comment
    First comment

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './navbar.css'
+const navbar = () => {
+  return (
+    <div className='nvbar'>
+      <p>Test nav</p>
+    </div>
+  )
+}
+
+export default navbar
